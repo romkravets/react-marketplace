bd706560340c7c53735bd27ef55330043e4b5c43
romkravets <romann.kravets@gmail.com>
2020-07-02 12:31:13 +0300

    svg icon react render

---
+++ src/Icon.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import T from 'prop-types';
+import { iconsConfig } from './iconsConfig';
+
+export const Icon = ({name, ...props}) => {
+   const IconC = iconsConfig[name];
+   return <IconC {...props}/>
+}
+
+Icon.propTypes = {
+   name: T.string.isRequired,
+   size: T.string,
+   color: T.string,
+}
+
+Icon.defaultProps = {
+   size: '14px',
+   color: '#0000'
+}
+
+
