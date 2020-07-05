683c427baaafacdb158f2e3b8afa6617c21469be
romkravets <romann.kravets@gmail.com>
2020-07-05 14:07:05 +0300

    useRef

--- src/ToDoInput.js
+++ src/ToDoInput.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import T from 'prop-types';
 
 export const ToDoInput = ({ onAdd, initialValue }) => {
    const [value, setInputValue] = useState(initialValue);
+
+   const textInput = useRef();
 
    const onChange = event => setInputValue(event.target.value);
 
@@ -13,9 +15,11 @@ export const ToDoInput = ({ onAdd, initialValue }) => {
       setInputValue('');
    }
 
+   useEffect(() => textInput.current.focus(),[]);
+
    return (
       <form onSubmit={onSubmit}>
-         <input {...{ value }} onChange={onChange} />
+         <input {...{ value }} onChange={onChange} ref={textInput}/>
       </form>
    )
 }
--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import T from 'prop-types';
 import { toDoListPropTypes } from './propTypes';
 import {Checkbox} from './Checkbox';
